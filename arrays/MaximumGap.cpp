db4fe06af0c4c9dfb6a733143b71bc16d880588d
harshit <harshitgp713@gmail.com>
2025-08-17 17:57:12 +0530

    Add solution: MaximumGap

---
+++ arrays/MaximumGap.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=1738495200 lang=cpp
+ *
+ * MaximumGap
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int n=nums.size();
+        
+        sort(nums.begin(), nums.end());
+        int i=1;
+        
+        int maxgap=0;
+        if(n<2){
+            return 0;
+        }
+while(i<n){
+     int gap=(nums[i]-nums[i-1]);
+     if(gap>maxgap){
+        maxgap=gap;
+     }
+     i++;
+    }
+
+return maxgap;
+        
+    }
+};
